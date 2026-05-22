7b89caccf05e3d8d3d2c30ff617d3ae145d94b5f
camilomosquera1608-afk <camilomosquera1608@gmail.com>
2026-05-21 19:23:22 -0500

    Corregir todo

--- app/secretaria-tecnica/jugadores/[id]/page.tsx
+++ app/secretaria-tecnica/jugadores/[id]/page.tsx
@@ -1,2 +1,10 @@
 import { TechnicalPlayerDetailPage } from '@/components/secretaria-tecnica/technical-pages';
-export default function Page({ params }: { params: { id: string } }) { return <TechnicalPlayerDetailPage playerId={params.id} />; }
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
+  return <TechnicalPlayerDetailPage playerId={id} />;
+}
