6fca13060f0c41cd105841ed3251bc0a98d22a0f
camilomosquera1608-afk <camilomosquera1608@gmail.com>
2026-07-28 16:56:18 -0500

    Fix: Corregir nombres de tablas en script SQL de competencia

--- sql/clear_competition_data.sql
+++ sql/clear_competition_data.sql
@@ -1,18 +1,14 @@
 -- Script para limpiar datos de competencia
 -- Este script elimina todos los datos de competencia manteniendo los jugadores
 
--- Limpiar CompetitionMatchSummaries
-DELETE FROM competition_match_summaries;
+-- Limpiar competition_matches
+DELETE FROM competition_matches;
 
--- Limpiar CompetitionRecords
-DELETE FROM competition_records;
-
--- Limpiar CompetitionLineupSlots
-DELETE FROM competition_lineup_slots;
+-- Limpiar competition_players
+DELETE FROM competition_players;
 
 -- Confirmar limpieza
 SELECT 'Datos de competencia limpiados exitosamente' as status;
 SELECT COUNT(*) as remaining_players FROM players;
-SELECT COUNT(*) as competition_match_summaries FROM competition_match_summaries;
-SELECT COUNT(*) as competition_records FROM competition_records;
-SELECT COUNT(*) as competition_lineup_slots FROM competition_lineup_slots;
+SELECT COUNT(*) as competition_matches FROM competition_matches;
+SELECT COUNT(*) as competition_players FROM competition_players;
